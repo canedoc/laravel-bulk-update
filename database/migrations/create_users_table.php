3b5ceb60f7abbec0f3ae14758cef968c7b209d1b
boubrit nacim <fn_boubrit@esi.dz>
2023-06-25 00:09:07 +0200

    add migration to create users test table

---
+++ database/migrations/create_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('users');
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->boolean('active')->default(true);
+        });
+    }
+};
