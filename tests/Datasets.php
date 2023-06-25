2f0101369790f528ac8ff5661152ad973a5ae822
boubrit nacim <fn_boubrit@esi.dz>
2023-06-25 22:19:27 +0200

    add test cases with null values

--- tests/Datasets.php
+++ tests/Datasets.php
@@ -1,7 +1,7 @@
 <?php
 
 dataset('users', [
-    'with single conditional update' => [
+    'with single column conditional update' => [
         [
             ['email' => 'naruto@best.com', 'name' => 'naruto', 'id' => 1],
             ['email' => 'sasuki@best.com', 'name' => 'sasuki', 'id' => 2],
@@ -11,22 +11,45 @@ dataset('users', [
             'email' => $result = [
                 ['email' => 'narutoisthebest@best.com', 'name' => 'naruto', 'id' => 1],
                 ['email' => 'sasukiisthebest@best.com', 'name' => 'sasuki', 'id' => 2],
-                ['email' => 'kakachiisthebest@best.com', 'name' => 'kakachi', 'id' => 2],
+                ['email' => null, 'name' => 'kakachi', 'id' => 2],
             ],
         ],
         $result,
     ],
 
-    'with set to values' => [
-        $baseData = [
+    'with single column conditional update when name is null' => [
+        [
+            ['email' => 'naruto@best.com', 'name' => 'naruto', 'id' => 1],
+            ['email' => 'sasuki@best.com', 'name' => null, 'id' => 2],
+        ],
+        [
+            'email' => $result = [
+                ['email' => 'narutoisthebest@best.com', 'name' => 'naruto', 'id' => 1],
+                ['email' => 'sasukiisthebest@best.com', 'name' => null, 'id' => 2],
+            ],
+        ],
+        $result,
+    ],
+
+    'with multiple column conditional update' => [
+        [
             ['email' => 'naruto@best.com', 'name' => 'naruto', 'id' => 1],
             ['email' => 'sasuki@best.com', 'name' => 'sasuki', 'id' => 2],
-            ['email' => 'kakachi@best.com', 'name' => 'kakachi', 'id' => 2],
         ],
-        $updateData = [
-            'email' => 'itachi@idotensi.com',
+        [
+            'email' => [
+                ['email' => 'narutoisthebest@best.com', 'name' => 'naruto', 'id' => 1],
+                ['email' => 'sasukiisthebest@best.com', 'name' => 'sasuki', 'id' => 2],
+                ['email' => 'kakachiisthebest@best.com', 'name' => 'kakachi', 'id' => 2],
+            ],
+            'name' => [
+                ['name' => 'Kurama', 'id' => 1],
+            ]
         ],
-        array_map(fn ($row) => $updateData + $row, $baseData),
+        [
+            ['email' => 'narutoisthebest@best.com', 'name' => 'Kurama', 'id' => 1],
+            ['email' => 'sasukiisthebest@best.com', 'name' => 'sasuki', 'id' => 2],
+        ]
     ],
 
     'with set to values' => [
