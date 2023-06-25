c01a6d982dc25df1768fbdf7fd8a314d5e9bfd67
boubrit nacim <fn_boubrit@esi.dz>
2023-06-25 22:49:34 +0200

    add tests using where clauses

--- tests/BulkUpdateTest.php
+++ tests/BulkUpdateTest.php
@@ -1,12 +1,31 @@
 <?php
 
-it('test update with conditional update', function ($data, $updateData, $result) {
+it('update with conditional update', function ($data, $updateData, $result, $filters = null) {
     \DB::table('users')->delete();
     \DB::table('users')->insert($data);
 
     \DB::table('users')->bulkUpdate($updateData);
 
-    expect(json_decode(\DB::table('users')->get()->values()->toJson(), true))
+    expect(getDBUsers())
         ->toHaveCount(count($result))
         ->toEqualCanonicalizing($result);
-})->with('users');
+})->with('users conditional update');
+
+
+it('update with filters clauses', function ($data, $updateData, $result, $filters) {
+    \DB::table('users')->insert($data);
+
+    $filters($query = \DB::table('users'));
+
+    $query->bulkUpdate($updateData);
+
+    expect(getDBUsers())
+        ->toHaveCount(count($result))
+        ->toEqualCanonicalizing($result);
+})->with('users conditional update with query filters');
+
+
+function getDBUsers(): array
+{
+    return json_decode(\DB::table('users')->get()->values()->toJson(), true);
+}
